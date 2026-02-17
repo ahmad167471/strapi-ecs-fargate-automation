9afce6db072726d9e28fd1c598ce7b86f7f31b28
Ahmad Amin <ahmad167471.com>
2026-02-18 02:59:18 +0530

    Fix Terraform backend region to ap-south-1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
-    key    = "terraform.tfstate"
-    region = "us-west-2"   # <- match the actual bucket region
+    bucket = "my-terraform-state-bucket"   # your bucket name
+    key    = "strapi/terraform.tfstate"
+    region = "ap-south-1"                  # match your bucket's region
   }
 }

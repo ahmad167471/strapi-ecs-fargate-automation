3bb0dbd818cdcdcd8ac70e398e0009c358b83be2
Ahmad Amin <ahmad167471.com>
2026-02-18 14:56:22 +0530

    fix backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"   # your bucket name
+    bucket = "my-terraform-state-bucket-ahmad"   # your bucket name
     key    = "strapi/terraform.tfstate"
     region = "us-east-1"                  # match your bucket's region
   }
